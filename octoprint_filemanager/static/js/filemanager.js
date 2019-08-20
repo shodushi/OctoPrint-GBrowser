0fed1404afa58f175eb33ba99917b2f38f52e337
Ron!n <stefan@kunter.org>
2019-08-20 21:36:56 +0200

    test

--- octoprint_filemanager/static/js/filemanager.js
+++ octoprint_filemanager/static/js/filemanager.js
@@ -396,6 +396,20 @@ $(function() {
 
             self.addFolderDialog = $("#add_folder_dialog");
         };
+
+        self.function zoomIn(event) {
+            var element = document.getElementById("overlay_d.gcode");
+            element.style.display = "inline-block";
+            var img = document.getElementById("imgZoom");
+            var posX = event.offsetX ? (event.offsetX) : event.pageX - img.offsetLeft;
+            var posY = event.offsetY ? (event.offsetY) : event.pageY - img.offsetTop;
+            element.style.backgroundPosition=(-posX*2)+"px "+(-posY*4)+"px";
+        }
+
+        self.function zoomOut() {
+            var element = document.getElementById("overlay_d.gcode");
+            element.style.display = "none";
+        }
     }
 
     OCTOPRINT_VIEWMODELS.push([
