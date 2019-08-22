f3db039c9a9769619a4089d982f6ca4ad96358e6
Ron!n <stefan@kunter.org>
2019-08-22 10:38:41 +0200

    test

--- octoprint_gbrowser/static/js/gbrowser.js
+++ octoprint_gbrowser/static/js/gbrowser.js
@@ -161,9 +161,7 @@ $(function() {
 */
             //return "/downloads/files/local/"+self.currentPath+"/"+data["name"].replace(".gcode", ".png");
             var path = "/downloads/files/local/"+data["name"].replace(".gcode", ".png");
-            if(self.currentPath() == "") {
-                path = "/downloads/files/local/"+data["name"].replace(".gcode", ".png");
-            } else {
+            if(self.currentPath() != "") {
                 path = "/downloads/files/local/"+self.currentPath()+"/"+data["name"].replace(".gcode", ".png");
             }
             return path;
